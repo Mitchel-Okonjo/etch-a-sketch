d59187fe4cdc4fca3a73fcf2de4406a4bdc34bbd
Mitchel-Okonjo <vokonjomitchel@gmai.com>
2022-10-19 14:47:06 +0100

    Add reset button functionality

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+const grid = document.querySelector('.grid');
+const defaultGrid = document.querySelector('.grid-text');
+const slider = document.querySelector('.slider');
+const defaultGridValue = slider.getAttribute('value');
+const apply = document.querySelector('.btn');
+const reset = document.querySelector('.fa-power-off');
+
+defaultGrid.textContent =  `${defaultGridValue}x${defaultGridValue}`;
+setGrid(defaultGridValue);
+
+
+grid.addEventListener('mouseover', function (e) {
+    if (e.target.matches('.box')) {
+        e.target.classList.add('active');
+    }
+});
+
+slider.addEventListener('input', function (e) {
+    defaultGrid.textContent = `${e.target.value}x${e.target.value}`;
+    
+});
+
+reset.addEventListener('click', () => resetGrid());
+
 function setGrid(n) {
     for (let i = 0; i < n; i++) {
         const div = document.createElement('div');
@@ -11,14 +35,9 @@ function setGrid(n) {
     }
 }
 
-const grid = document.querySelector('.grid');
-const defaultGrid = document.querySelector('.grid-text');
-const slider = document.querySelector('.slider');
-const gridValue = slider.getAttribute('value');
-
-defaultGrid.textContent =  `${gridValue}x${gridValue}`;
-setGrid(gridValue);
-
-const boxes = document.querySelectorAll('.box');
-
-boxes.forEach(box => box.addEventListener('mouseover', () => box.classList.add('active')));
+function resetGrid() {
+  while (grid.firstChild) {
+    grid.removeChild(grid.lastChild);
+  }
+  setGrid(defaultGridValue);
+}
