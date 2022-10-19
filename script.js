f28e38deae80bb8a5b9a8393946f13ff369c5138
Mitchel-Okonjo <vokonjomitchel@gmai.com>
2022-10-19 10:30:08 +0100

    Set default grid

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-const grid = document.querySelector('.grid');
-const n = 64;
-for (let i = 0; i < n; i++) {
-    const div = document.createElement('div');
-    div.classList.add('row');
-    for (let j = 0; j < n; j++) {
-        const childDiv = document.createElement('div');
-        childDiv.classList.add('box');
-        div.appendChild(childDiv);
+function setGrid(n) {
+    for (let i = 0; i < n; i++) {
+        const div = document.createElement('div');
+        div.classList.add('row');
+        for (let j = 0; j < n; j++) {
+            const childDiv = document.createElement('div');
+            childDiv.classList.add('box');
+            div.appendChild(childDiv);
+        }
+        grid.appendChild(div);
     }
-    grid.appendChild(div);
 }
 
+const grid = document.querySelector('.grid');
+const defaultGrid = document.querySelector('.grid-text');
+const slider = document.querySelector('.slider');
+const gridValue = slider.getAttribute('value');
+
+defaultGrid.textContent =  `${gridValue}x${gridValue}`;
+setGrid(gridValue);
+
 const boxes = document.querySelectorAll('.box');
+
 boxes.forEach(box => box.addEventListener('mouseover', () => box.classList.add('active')));
